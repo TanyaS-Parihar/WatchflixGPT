7875c188d7510d6ca2900365259a9deadc8d790a
TanyaS_Parihar <tanyasp666@gmail.com>
2024-06-29 19:43:55 +0530

    "Second Gpt page"

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -5,7 +5,6 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 // Fetch your API_KEY
 const API_KEY = "AIzaSyA1UkMbD_YBGrB9G818YwLaTzaJStERmD4";
 
-// Access your API key (see "Set up your API key" above)
 export const genAI = new GoogleGenerativeAI(API_KEY);
 
 export const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
